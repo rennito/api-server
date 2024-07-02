7f9a83bf49887b9e2b18f5d23001e69e83e5eb99
Jose Rodolfo <loco_2189@hotmail.com>
2024-07-02 12:31:51 -0700

    se agregan cambios

--- api/server.js
+++ api/server.js
@@ -1,29 +1,26 @@
-const jsonServer = require('json-server');
-const cors = require('cors');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
 
 // Middlewares por defecto de json-server (logger, static, etc.)
 server.use(middlewares);
 
 // Habilitar CORS con configuración específica
-server.use(cors({
+/*server.use(cors({
     origin: ['http://localhost:3000', 'https://proyecto-react-git-master-rennitos-projects.vercel.app'], // Permitir solo este origen
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
   allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
-}));
+}));*/
 
-// Rutas personalizadas o reescrituras de URL
 server.use(jsonServer.rewriter({
-  '/api/*': '/$1', // Reescribe /api/* a /*
-  '/product/:resource/:id/show': '/:resource/:id', // Ejemplo de reescritura
-}));
-
-// Montar el enrutador JSON
-server.use(router);
-
-// Iniciar el servidor en el puerto 3000
+    '/api/*': '/$1',
+    '/product/:resource/:id/show': '/:resource/:id'
+}))
+server.use(router)
 server.listen(5000, () => {
-  console.log('JSON Server is running');
-});
+    console.log('JSON Server is running')
+})
+
+// Export the Server API
+module.exports = server
